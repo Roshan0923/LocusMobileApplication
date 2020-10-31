7a46eec8c3acf212465c28745c77e2db83a20407
Roshan Kirtikumar Patel <rs735202@dal.ca>
2020-10-31 13:02:57 -0300

    Added List of Availble seats Activity

--- app/src/main/java/com/example/locus/AirportInformationActivity.java
+++ app/src/main/java/com/example/locus/AirportInformationActivity.java
@@ -2,13 +2,32 @@ package com.example.locus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AirportInformationActivity extends AppCompatActivity {
+
+    private Button airportInfoProccedButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_airport_information);
+        airportInfoProccedButton=(Button)findViewById(R.id.airportInfoProcced);
+
+        airportInfoProccedButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                proccedToAvailBleSetsAvtivity();
+            }
+        });
+    }
+
+    public void proccedToAvailBleSetsAvtivity()
+    {
+        Intent intent=new Intent(this,ListOfPlacesAvailable.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/locus/ListOfPlacesAvailable.java
@@ -0,0 +1,14 @@
+package com.example.locus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListOfPlacesAvailable extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_of_places_available);
+    }
+}
