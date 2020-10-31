690ebc5d1534d313e4d48b756a1bc76bead679b7
Roshan Kirtikumar Patel <rs735202@dal.ca>
2020-10-31 12:15:29 -0300

    Added Login and Registration Page

---
+++ app/src/main/java/com/example/locus/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.locus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button registerButton;
+    private Button loginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        registerButton=(Button)findViewById(R.id.register);
+        loginButton=(Button)findViewById(R.id.loginbtm);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openRestrationPage();
+            }
+        });
+
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openAirportInfo();
+            }
+        });
+
+    }
+
+    public void openRestrationPage(){
+        Intent intent=new Intent(this,Register.class);
+        startActivity(intent);
+    }
+    public void openAirportInfo(){
+        Intent intent=new Intent(this,AirportInformationActivity.class);
+        startActivity(intent);
+    }
+}
